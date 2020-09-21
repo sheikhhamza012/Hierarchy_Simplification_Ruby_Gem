766b90ce7cc1c25e1e7ffd1c7e144e9ae798f616
sheikh hamza <sheikhhamza012@gmail.com>
2020-09-21 23:27:47 +0500

    tassk 2

--- lib/hierarchy_simplification.rb
+++ lib/hierarchy_simplification.rb
@@ -75,7 +75,62 @@ class HierarchySimplification
         end
     end
     puts "writing file..."
-    input.write("file3.xlsx")
+    input.write(file_path)
 
   end
+
+
+  def self.rename_cells(file_path, parents_path)
+        parents = []
+        nodes = []
+        new_names = []
+        puts"opening file with new names.."
+        input = RubyXL::Parser.parse(parents_path)
+        
+        puts"reading values to change.."
+        input[0].drop(1).each_with_index do |row,row_index| 
+            next if row[3]&.value.nil?
+            nodes.push(row[0].value)
+            parents.push(row[1].value)
+            new_names.push(row[3].value)
+        end
+        
+        puts"opening file to change.."
+        input = RubyXL::Parser.parse(file_path)
+        input[0].drop(1).each_with_index do |row,row_index| 
+            i=input[0][0].size-1
+            # byebug if row_index==4
+            while i>0 do
+                if !row[i]&.value.nil?
+                    node = row[i].value
+                    parent = row[i-1]&.value
+                    k=i-1
+                    while k>0
+                            if !row[k]&.value.nil?
+                                parent = row[k].value
+                                break
+                            end
+                            k-=1
+                    end
+                    searched = nodes.each_index.select{|n| nodes[n] == node && parents[n] == parent}
+                    if searched.size>0
+                            puts "changing "+row[i].value.to_s+" to "+new_names[searched.first]
+                            if row[i].value == row[input[0][0].size-1].value && i!=input[0][0].size-1
+                                row[input[0][0].size-1].change_contents(new_names[searched.first])
+                            end
+                            value =row[i]&.value
+                            k=i
+                            while row[k]&.value == value
+                                row[k].change_contents(new_names[searched.first])
+                                k+=1
+                            end
+                        end
+                end
+                i-=1;
+            end
+        end
+        puts"saving.."
+        input.write(file_path)
+        
+    end
 end
